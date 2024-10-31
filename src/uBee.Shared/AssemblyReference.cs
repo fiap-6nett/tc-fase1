564dc17de5d2189aac24ead6ae33be1bff7f58f3
luqonhas <apolinariodev@gmail.com>
2024-10-31 01:34:21 -0300

    fix: remoção da pasta Shared que faltou remover junto com a remoção da camada

--- src/uBee.Shared/AssemblyReference.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-
-namespace uBee.Shared
-{
-    public static class AssemblyReference
-    {
-        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-    }
-}
